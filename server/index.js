9d6ef5de5cd8879c7b02a140e70372e24971b15d
Hrvoje Šimić <shime@twobucks.co>
2017-07-30 12:38:19 +0200

    replace vars with consts

--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('x-powered-by', false)
 app.use('/public', express.static(path.join(__dirname, '../public')))
 
-var server = app.listen(3000, function () {
-  var port = server.address().port
+const server = app.listen(3000, function () {
+  const port = server.address().port
 
-  console.log('Example app listening at http://127.0.0.1:%s', port)
+  console.log('Server listening at http://127.0.0.1:%s', port)
 })
