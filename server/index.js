59cf5120e8315f9feff28a9a51c96c8dcb862bea
Hrvoje Šimić <shime@twobucks.co>
2017-08-06 18:37:30 +0200

    add emoji favicon

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const compression = require('compression')
 const path = require('path')
+const emojiFavicon = require('emoji-favicon')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(compression())
+app.use(emojiFavicon('sparkles'))
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../public', 'index.html'))
