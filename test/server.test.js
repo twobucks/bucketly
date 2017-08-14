0ffca7e0c88f3942a314a39067ab0c6dbf057af0
Hrvoje Šimić <shime@twobucks.co>
2017-08-14 10:07:08 +0200

    revert "stop testing react builds"

--- test/server.test.js
+++ test/server.test.js
@@ -1,8 +1,19 @@
 // test/server.test.js
+const exec = require('mz/child_process').exec
 const request = require('supertest-as-promised')
 const expect = require('chai').expect
 
 const app = require('../server')
+
+describe('builds application', function () {
+  it('builds to "build" directory', function () {
+    // Disable mocha time-out because this takes a lot of time
+    this.timeout(0)
+
+    // Run process
+    return exec('cd client && npm run build')
+  })
+})
 
 describe('express serving', function () {
   it('responds to / with the index.html', function () {
@@ -34,7 +45,7 @@ describe('express serving', function () {
     .attach('file', 'test/cat.jpg')
     .expect('Content-Type', /application\/json/)
     .expect(500)
-    .then(res => expect(JSON.parse(res.text).error).to.contain('Missing credentials'))
+    .then(res => expect(JSON.parse(res.text).error).to.contain('The requested bucket name is not available.'))
   }).timeout(0)
 
   it('responds to POST on /api/test', () => {
