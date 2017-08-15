4fd99f2b0844fe0f6b65f95dc7c6d92129730c39
Hrvoje Šimić <shime@twobucks.co>
2017-08-15 10:46:22 +0200

    add routes test

--- client/src/routes.test.js
+++ client/src/routes.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Routes from './routes'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<Routes />, div)
 })
