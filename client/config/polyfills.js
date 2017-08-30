ea4857fb37eb77e950d40c12603fd00bfc632347
Hrvoje Šimić <shime@twobucks.co>
2017-08-30 15:48:41 +0200

    add polyfill for local storage

--- client/config/polyfills.js
+++ client/config/polyfills.js
@@ -14,3 +14,23 @@ require('whatwg-fetch');
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.
 Object.assign = require('object-assign');
+
+var localStorageMock = (function() {
+  var store = {};
+  return {
+    getItem: function(key) {
+      return store[key];
+    },
+    setItem: function(key, value) {
+      store[key] = value.toString();
+    },
+    clear: function() {
+      store = {};
+    },
+    removeItem: function(key) {
+      delete store[key];
+    }
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
