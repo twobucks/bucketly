0a711d2d3642c2e60db5fe04543028d6d7331771
Hrvoje Šimić <shime@twobucks.co>
2017-08-31 18:58:32 +0200

    add underscored columns to users and images

---
+++ server/migrations/20170831164552-add-underscored-columns.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    queryInterface.renameColumn('users', 'createdAt', 'created_at')
+    queryInterface.renameColumn('users', 'updatedAt', 'updated_at')
+    queryInterface.renameColumn('images', 'createdAt', 'created_at')
+    return queryInterface.renameColumn('images', 'updatedAt', 'updated_at')
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+  },
+
+  down: function (queryInterface, Sequelize) {
+    queryInterface.renameColumn('users', 'created_at', 'createdAt')
+    queryInterface.renameColumn('users', 'updated_at', 'updatedAt')
+    queryInterface.renameColumn('images', 'created_at', 'createdAt')
+    return queryInterface.renameColumn('images', 'updated_at', 'updatedAt')
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
